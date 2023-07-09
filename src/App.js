6c4534b687cd018f3dc559a831b50aea9a10f45e
Lauren Johnston <hilotraining@outlook.com>
2023-07-09 16:29:51 -0600

    added candy jar example

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import CandyJar from './components/CandyJar';
+
+
+function App() {
+  return (
+    <CandyJar />
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//WEEK 1
+//import Card from './components/Card';
+// function App() {
+//   return (
+//     <Card
+//       title="React Component"
+//       description="This is a sample card component."
+//     >
+//       <p>This is some additional information about the card.</p>
+//       <button>Click Me</button>
+//     </Card>
+//   );
+// }
+
+// export default App;
---
+++ src/components/CandyJar.jsx
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react'
+import './CandyJar.css';
+
+const CandyJar = () => {
+  const [count, setCount] = useState(5);
+  const [color, setColor] = useState('pink')
+
+  useEffect(() => {
+    alert(`Color has been updated to: ${color}`)
+  }, [color])
+
+  const eatCandy = () => {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  const addCandy = () => {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  const handleColorChange = (e) => {
+    setColor(e.target.value)
+  }
+
+  return (
+    <div className='jar'>
+      <h2>Candy Jar</h2>
+      <div className="circle-container">
+        {
+          Array.from({ length: count }, (_, index) => (
+            <div key={index} className={`circle ${color}`}></div>
+          ))
+        }
+      </div>
+      <h4> Total Candies: {count}</h4>
+
+      <div>
+        <button onClick={eatCandy}>Eat Candy</button>
+        <button onClick={addCandy}>Add Candy</button>
+      </div>
+
+      <div>
+        <label htmlFor="colorSelect">Select Color:</label>
+        <select id="colorSelect" value={color} onChange={handleColorChange}>
+          <option value="pink">Pink</option>
+          <option value="green">Green</option>
+          <option value="purple">Purple</option>
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default CandyJar
